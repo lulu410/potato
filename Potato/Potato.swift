e9bb6ec662f7298532da8d78f6d7b4ad80b504ef
myo3 <myo3@users.noreply.github.com>
2016-02-13 15:06:10 -0500

    Created backend classes to manage data

---
+++ Potato/Potato.swift
@@ -0,0 +1,13 @@
+//
+//  Potato.swift
+//  Potato
+//
+//  Created by Monica Ong on 2/13/16.
+//  Copyright © 2016 Monica Ong. All rights reserved.
+//
+
+import UIKit
+
+class Potato: NSObject {
+
+}
